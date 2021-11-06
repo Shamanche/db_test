1ea7f6c6267092dfe73b2981159eb0ffdd7f8401
Shamanche <72706516+Shamanche@users.noreply.github.com>
2021-11-06 23:28:26 +0300

    initial 2

--- main.py
+++ main.py
@@ -1,37 +1,24 @@
 from peewee import *
 from datetime import date
 
-db = SqliteDatabase('people.db')
+db = SqliteDatabase('backgamon.db')
 
-class Person(Model):
+class Player(Model):
     name = CharField()
-    birthday = DateField()
-    is_relative = BooleanField()
+    viber_id = CharField()
+    bitrix_id = IntegerField()
+    is_deleted = BooleanField(default=False)
     class Meta:
         database = db
 
-class Pet(Model):
-    owner = ForeignKeyField(Person, related_name='pets')
+class Game(Model):
     name = CharField()
-    animal_type = CharField()
+    date = DateField()
+    winner = ForeignKeyField(Player, related_name='players')
+    loser = ForeignKeyField(Player, related_name='players')
+    win_type = CharField()
     class Meta:
-        database = db
+        Database = db
 
 db.connect()
-db.create_tables([Person, Pet])
-
-uncle_bob = Person(name='Bob', birthday=date(1960, 1, 15), is_relative = True)
-uncle_bob.save()
-
-grandma = Person.create(name='Grandma', birthday=date(1935, 3, 1), is_relative = True)
-herb = Person.create(name='Herb', birthday=date(1950, 5, 5), is_relative = False)
-
-bob_kitty = Pet.create(owner=uncle_bob, name='Kitty', animal_type='cat')
-herb_fido = Pet.create(owner=herb, name='Fido', animal_type='dog')
-herb_mittens = Pet.create(owner=herb, name='Mittens', animal_type='cat')
-herb_mittens_jr = Pet.create(owner=herb, name='Mittens Jr', animal_type='cat')
-
-
-
-
-
+db.create_tables([Player, Game])
